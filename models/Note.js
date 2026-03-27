f1637c54cca8e8f3d724332082a5a19854cd0a6e
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-03-27 21:22:49 +0530

    Created Note Schema

---
+++ models/Note.js
@@ -0,0 +1,65 @@
+const mongoose=require("mongoose");
+
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please provide a title for the note"],
+      minlength: 2,
+      maxlength: 100,
+      trim: true,
+    },
+
+    content: {
+      type: String,
+      required: [true, "Please provide content for the note"],
+      minlength: 2,
+      maxlength: 1000,
+      trim: true,
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    // 🏷️ Tags (for filtering/search)
+    tags: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+
+    // 📌 Pin important notes
+    pinned: {
+      type: Boolean,
+      default: false,
+    },
+
+    // 🗂️ Soft delete (important for real apps)
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+
+    // 🔐 Private / Public note
+    isPublic:{
+        type:Boolean,
+        default:false
+    }
+  },
+  {
+    timestamps: true,
+  },
+);
+
+noteSchema.index({title:"text",content:"text"},{
+    weights:{
+        title:5,
+        content:1
+    }
+});
+
+module.exports=mongoose.model("Note",noteSchema);
