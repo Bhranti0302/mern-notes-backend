30b0a643a86572e3093664ba12796f3899e8384f
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-06-06 22:49:56 +0530

    feat: enhance User model with authentication methods
    - Added comparePassword method
    - Integrated bcrypt for password validation
    - Improved user authentication flow

---
+++ models/User.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Please add a name'],
+            trim: true,
+            minlength: 3
+        },
+
+        email: {
+            type: String,
+            required: [true, 'Please add an email'],
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please add a valid email']
+        },
+
+        password: {
+            type: String,
+            required: [true, 'Please add a password'],
+            minlength: 6,
+            select: false
+        },
+
+        role: {
+            type: String,
+            enum: ['user', 'admin'],
+            default: 'user',
+        },
+
+        status: {
+            type: String,
+            enum: ["online", "offline"],
+            default: "offline"
+        },
+
+        lastSeen: {
+            type: Date,
+        }
+    },{timestamps: true}
+)
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return;
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+module.exports = mongoose.model('User', UserSchema);
