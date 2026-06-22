398ab59e19df6ed5f48b86d54eec9506dd5a0695
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-06-22 21:27:34 +0530

    feat: create Note model with schema fields and references

---
+++ models/Note.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const noteSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, "Please add a title"],
+        },
+
+        content: {
+            type: String,
+            required: [true, "Please add a content"],
+        },
+
+        tags: [{
+            type: String,
+            trim: true
+        }],
+
+        collection: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Collection",
+            required: true
+        },
+
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+
+        isPinned: {
+            type: Boolean,
+            default: false
+        },
+
+        isArchived: {
+            type: Boolean,
+            default: false
+        },
+
+    },
+    {
+        timestamps: true,
+    }
+)
+
+module.exports = mongoose.model("Note", noteSchema);
