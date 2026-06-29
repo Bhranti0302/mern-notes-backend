2591768835489de40538c9ad95192cfc7f1dd423
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-06-29 17:10:21 +0530

    fix(middleware): export notFound and errorHandler correctly and improve error handling

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,15 +1,19 @@
 // Not Found
 const notFound = (req, res, next) => {
-    res.status(404);
-    throw new Error(`Not Found - ${req.originalUrl}`);
-}
+  res.status(404);
+  throw new Error(`Not Found - ${req.originalUrl}`);
+};
 
 // Error Handler
 const errorHandler = (err, req, res, next) => {
-    res.status(err.statusCode || 500).json({
-        success: false,
-        error: err.message
-    })
-}
+  res.status(err.statusCode || 500).json({
+    success: false,
+    error: err.message,
+  });
+};
 
-module.exports = errorHandler
+
+module.exports = {
+  notFound,
+  errorHandler,
+};
