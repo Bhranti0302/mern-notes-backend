a3ceb8d75482c556e33a5bb7e8b8566e26c2a5ab
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-06-29 17:10:32 +0530

    feat(model): update Note schema with tags, collection reference, and user association

--- models/Note.js
+++ models/Note.js
@@ -1,48 +1,49 @@
 const mongoose = require("mongoose");
 
 const noteSchema = mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: [true, "Please add a title"],
-        },
-
-        content: {
-            type: String,
-            required: [true, "Please add a content"],
-        },
-
-        tags: [{
-            type: String,
-            trim: true
-        }],
-
-        collection: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Collection",
-            required: true
-        },
-
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true
-        },
-
-        isPinned: {
-            type: Boolean,
-            default: false
-        },
-
-        isArchived: {
-            type: Boolean,
-            default: false
-        },
-
+  {
+    title: {
+      type: String,
+      required: [true, "Please add a title"],
     },
-    {
-        timestamps: true,
-    }
-)
+
+    content: {
+      type: String,
+      required: [true, "Please add a content"],
+    },
+
+    tags: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+
+    collection: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Collection",
+      default: null,
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    isPinned: {
+      type: Boolean,
+      default: false,
+    },
+
+    isArchived: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 module.exports = mongoose.model("Note", noteSchema);
